b91195a1506bfd3e62281bcf54cbf56618a75877
gustavovalle23 <gustavo.vallerp@hotmail.com>
2020-11-04 11:10:17 -0300

    Analise dos pontos faciais em tempo real

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import dlib
 
 # Load the detector
@@ -9,35 +8,42 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 # read the image
-img = cv2.imread("face.jpg")
+cap = cv2.VideoCapture(0)
 
-# Convert image into grayscale
-gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+while True:
+    _, frame = cap.read()
+    # Convert image into grayscale
+    gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
 
-# Use detector to find landmarks
-faces = detector(gray)
-for face in faces:
-    x1 = face.left() # left point
-    y1 = face.top() # top point
-    x2 = face.right() # right point
-    y2 = face.bottom() # bottom point
+    # Use detector to find landmarks
+    faces = detector(gray)
 
-    # Create landmark object
-    landmarks = predictor(image=gray, box=face)
+    for face in faces:
+        x1 = face.left()  # left point
+        y1 = face.top()  # top point
+        x2 = face.right()  # right point
+        y2 = face.bottom()  # bottom point
 
-    # Loop through all the points
-    for n in range(0, 68):
-        x = landmarks.part(n).x
-        y = landmarks.part(n).y
+        # Create landmark object
+        landmarks = predictor(image=gray, box=face)
 
-        # Draw a circle
-        cv2.circle(img=img, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
+        # Loop through all the points
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
 
-# show the image
-cv2.imshow(winname="Face", mat=img)
+            # Draw a circle
+            cv2.circle(img=frame, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
 
-# Delay between every fram
-cv2.waitKey(delay=0)
+    # show the image
+    cv2.imshow(winname="Face", mat=frame)
+
+    # Exit when escape is pressed
+    if cv2.waitKey(delay=1) == 27:
+        break
+
+# When everything done, release the video capture and video write objects
+cap.release()
 
 # Close all windows
 cv2.destroyAllWindows()
